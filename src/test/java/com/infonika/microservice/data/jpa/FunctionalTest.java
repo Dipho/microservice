8bd68b5cf3247eaba427a80bff9e7fe29f487700
dipho <dani_game_17@hotmail.com>
2018-07-08 17:50:22 -0300

    Specified test port to 8080

--- src/test/java/com/infonika/microservice/data/jpa/FunctionalTest.java
+++ src/test/java/com/infonika/microservice/data/jpa/FunctionalTest.java
@@ -9,7 +9,7 @@ public class FunctionalTest {
 	public static void setup() {
 		String port = System.getProperty("server.port");
 		if(port == null) {
-			RestAssured.port = Integer.valueOf(9090);
+			RestAssured.port = Integer.valueOf(8080);
 		}
 		else {
 			RestAssured.port = Integer.valueOf(port);
